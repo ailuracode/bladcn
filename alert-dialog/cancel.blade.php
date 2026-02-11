ec462b5d8b063e76e88535c936ba435829bb2ba8
Pablo Santiago Guerra <livesanty_@hotmail.com>
2026-02-11 07:42:56 +0000

    Add `<x-card />` component

--- alert-dialog/cancel.blade.php
+++ alert-dialog/cancel.blade.php
@@ -3,7 +3,7 @@
     'size' => 'default',
 ])
 
-<x-button as-child="true" size="{{ $size }}" variant="{{ $variant }}" x-on:click="open = false">
+<x-button size="{{ $size }}" variant="{{ $variant }}" x-on:click="open = false">
     <div {{ $attributes->twMerge('') }} data-slot="alert-dialog-cancel">
         {{ $slot }}
     </div>
---
+++ card/content.blade.php
@@ -0,0 +1,3 @@
+<div {{ $attributes->twMerge('px-4 group-data-[size=sm]/card:px-3') }} data-slot="card-content">
+    {{ $slot }}
+</div>
