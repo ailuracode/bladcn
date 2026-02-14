70f3946abf6b0c9fe62decc3d83f078c6906cc17
Pablo Santiago Guerra <livesanty_@hotmail.com>
2026-02-14 00:51:57 +0000

    Refactor button group text component to use 'tag' attribute instead of 'as' for consistency

--- button-group/text.blade.php
+++ button-group/text.blade.php
@@ -2,8 +2,9 @@
     'as' => 'div',
 ])
 
-<x-as-child :as="$as"
+<x-as-child
     {{ $attributes->twMerge("bg-muted gap-2 rounded-lg border px-2.5 text-sm font-medium [&_svg:not([class*='size-'])]:size-4 flex items-center [&_svg]:pointer-events-none") }}
-    data-slot="button-group-text">
+    data-slot="button-group-text"
+    tag='{{ $as }}'>
     {{ $slot }}
 </x-as-child>
